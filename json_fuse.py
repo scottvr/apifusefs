59d3f6f0e875441e514084187c2a2ebb09837646
ScottVR <scottvr@gmail.com>
2026-03-04 17:20:12 -0600

    cruft removal machine

--- json_fuse.py
+++
@@ -1,3 +0,0 @@
-from apifuse.providers.json import JSONFuse, JSONProvider
-
-__all__ = ["JSONFuse", "JSONProvider"]
--- provider_fuse.py
+++
@@ -1,3 +0,0 @@
-from apifuse.fuse_ops import FilesystemProvider, ProviderError, ProviderFuse, ProviderNode
-
-__all__ = ["FilesystemProvider", "ProviderError", "ProviderFuse", "ProviderNode"]
